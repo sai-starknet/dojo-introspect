73559084b74d9eddce6b96b51aeb437075ee43f5
Bengineer <blockengineer42@gmail.com>
2025-10-16 19:43:52 +0100

    refactor: move empty_call and is_legacy functions out of DojoSchemaFetcher trait

--- crates/types/src/contract.rs
+++ crates/types/src/contract.rs
@@ -23,58 +23,54 @@ pub enum DojoSchemaFetcherError {
     InvalidSchema,
 }
 
-pub trait DojoSchemaFetcher {
-    fn empty_call(
-        &self,
-        contract_address: Felt,
-        entry_point: Felt,
-    ) -> impl Future<Output = Result<Vec<Felt>, ProviderError>>;
-    fn is_legacy(
-        &self,
-        contract_address: Felt,
-    ) -> impl Future<Output = Result<bool, DojoSchemaFetcherError>>;
-    fn schema(
-        &self,
-        contract_address: Felt,
-    ) -> impl Future<Output = Result<StructDef, DojoSchemaFetcherError>>;
+async fn empty_call(
+    provider: &impl Provider,
+    contract_address: Felt,
+    entry_point: Felt,
+) -> Result<Vec<Felt>, ProviderError> {
+    let call = FunctionCall {
+        contract_address,
+        entry_point_selector: entry_point,
+        calldata: vec![],
+    };
+
+    Ok(provider
+        .call(call, BlockId::Tag(BlockTag::PreConfirmed))
+        .await?)
 }
 
-impl<P> DojoSchemaFetcher for P
+async fn is_legacy(
+    provider: &impl Provider,
+    contract_address: Felt,
+) -> Result<bool, DojoSchemaFetcherError> {
+    let legacy_call = empty_call(
+        provider,
+        contract_address,
+        USE_LEGACY_STORAGE_ENTRYPOINT_SELECTOR,
+    );
+    match legacy_call.await {
+        Ok(felts) => match felts.len() {
+            1 => Ok(felts[0].is_one()),
+            _ => Err(DojoSchemaFetcherError::InvalidLegacyResponse),
+        },
+        Err(ProviderError::StarknetError(StarknetError::EntrypointNotFound)) => Ok(false),
+        Err(err) => Err(DojoSchemaFetcherError::ProviderError(err)),
+    }
+}
+
+pub trait DojoSchemaFetcher<Err> {
+    fn schema(&self, contract_address: Felt) -> impl Future<Output = Result<StructDef, Err>>;
+}
+
+impl<P> DojoSchemaFetcher<DojoSchemaFetcherError> for P
 where
     P: Provider,
 {
-    async fn empty_call(
-        &self,
-        contract_address: Felt,
-        entry_point: Felt,
-    ) -> Result<Vec<Felt>, ProviderError> {
-        let call = FunctionCall {
-            contract_address,
-            entry_point_selector: entry_point,
-            calldata: vec![],
-        };
-        Ok(self
-            .call(call, BlockId::Tag(BlockTag::PreConfirmed))
-            .await?)
-    }
-
-    async fn is_legacy(&self, contract_address: Felt) -> Result<bool, DojoSchemaFetcherError> {
-        let legacy_call = self.empty_call(contract_address, USE_LEGACY_STORAGE_ENTRYPOINT_SELECTOR);
-        match legacy_call.await {
-            Ok(felts) => match felts.len() {
-                1 => Ok(felts[0].is_one()),
-                _ => Err(DojoSchemaFetcherError::InvalidLegacyResponse),
-            },
-            Err(ProviderError::StarknetError(StarknetError::EntrypointNotFound)) => Ok(false),
-            Err(err) => Err(DojoSchemaFetcherError::ProviderError(err)),
-        }
-    }
-
     async fn schema(&self, contract_address: Felt) -> Result<StructDef, DojoSchemaFetcherError> {
-        let schema_call = self.empty_call(contract_address, SCHEMA_ENTRYPOINT_SELECTOR);
-        let legacy_call = self.is_legacy(contract_address);
-        let legacy = legacy_call.await?;
-        let schema_call_result = match schema_call.await {
+        let schema_call = empty_call(self, contract_address, SCHEMA_ENTRYPOINT_SELECTOR).await;
+        let legacy_call = is_legacy(self, contract_address).await;
+        let legacy = legacy_call?;
+        let schema_call_result = match schema_call {
             Ok(felts) => felts,
             Err(err) => return Err(DojoSchemaFetcherError::ProviderError(err)),
         };
